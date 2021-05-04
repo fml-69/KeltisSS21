be42aa079ff1525153a1f17d229a1e4212f3c59b
Fatima Elichanova <elichanova@gmail.com>
2021-05-04 12:29:09 +0200

    Added DesktopLauncher and EntryScene

---
+++ core/src/main/java/com/groupd/keltis/scenes/login/EntryScene.java
@@ -0,0 +1,36 @@
+package com.groupd.keltis.scenes.login;
+
+import com.groupd.keltis.Keltis;
+import com.groupd.keltis.scenes.AbstractScene;
+
+public class EntryScene extends AbstractScene {
+
+    public EntryScene(Keltis keltis) {
+        super(keltis);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
