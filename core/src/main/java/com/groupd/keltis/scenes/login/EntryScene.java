69b30bdf339ba6c75bd7b4cc9d0d939aebdcf967
Fatima Elichanova <elichanova@gmail.com>
2021-05-05 16:41:11 +0200

    Added select screen to EntryScene

--- core/src/main/java/com/groupd/keltis/scenes/login/EntryScene.java
+++ core/src/main/java/com/groupd/keltis/scenes/login/EntryScene.java
@@ -1,39 +1,37 @@
 package com.groupd.keltis.scenes.login;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.groupd.keltis.Keltis;
 import com.groupd.keltis.management.SceneManager;
 import com.groupd.keltis.scenes.AbstractScene;
+import com.groupd.keltis.utils.AssetPaths;
 
 public class EntryScene extends AbstractScene {
 
     private Stage stage;
     private Button startButton;
+    private TextField text;
+    private Label errorLabel;
 
     public EntryScene(Keltis keltis) {
         super(keltis);
+
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-        style.font = keltis.font;
-        //stage.addActor(new TextButton("Enter", style));
-        startButton = new TextButton("Start", style);
-        stage.addActor(startButton);
-        startButton.addListener(new InputListener(){
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-               keltis.sceneManager.setScene(SceneManager.GAMESTATE.PLAYING);
-               return true;
-            }
-        });
+
+
     }
 
     @Override
@@ -44,6 +42,57 @@ public class EntryScene extends AbstractScene {
     @Override
     public void show() {
 
+        Skin skin = keltis.assetManager.get(AssetPaths.UI_SKIN);
+
+        VerticalGroup vg = new VerticalGroup().space(3).pad(5).fill();
+        vg.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.addActor(vg);
+
+
+        Label label = new Label("Enter your name", skin);
+        //stage.addActor(label);
+        vg.addActor(label);
+
+        text = new TextField("", skin);
+        text.addListener(new InputListener(){
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) {
+                if(keycode == Input.Keys.ENTER){
+                    requestEntry();
+                    return true;
+                }
+                return super.keyDown(event, keycode);
+            }
+        });
+
+        vg.addActor(text);
+
+        startButton = new TextButton("Start", skin);
+        //stage.addActor(startButton);
+        vg.addActor(startButton);
+
+        errorLabel = new Label("", skin);
+        vg.addActor(errorLabel);
+
+
+        startButton.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                requestEntry();
+                return true;
+            }
+        });
+
+    }
+
+    private void requestEntry() {
+        if (text.getText().isEmpty()) {
+            errorLabel.setText("No name, no game.");
+        }
+        else{
+            keltis.sceneManager.setScene(SceneManager.GAMESTATE.PLAYING);
+
+        }
     }
 
     @Override
