155fe4778a03bb502e13be7dc32d7349f9eca7fc
fml-69 <gsauer22@gmail.com>
2021-05-11 22:20:14 +0200

    added card stack positions to PositioningConstants class; added Card actors to Board class

--- core/src/main/java/com/groupd/keltis/utils/PositioningConstants.java
+++ core/src/main/java/com/groupd/keltis/utils/PositioningConstants.java
@@ -1,25 +1,22 @@
 package com.groupd.keltis.utils;
 
-import java.awt.Point;
+import com.badlogic.gdx.math.GridPoint2;
 
 public final class PositioningConstants {
     private PositioningConstants(){}
 
-    public static Point[][] playerOffsetValues = {
-            {new Point(655,830), new Point(619,802), new Point(583,767),
-                    new Point(960,930), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0)},
-            {new Point(0, 0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0)},
-            {new Point(0, 0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0)},
-            {new Point(0, 0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0)},
-            {new Point(0, 0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0),
-                    new Point(0,0), new Point(0,0), new Point(0,0)}
-    };
+    public static final GridPoint2 CARD_BRANCHSTACK_GREEN = new GridPoint2(1720, 880);
+    public static final GridPoint2 CARD_BRANCHSTACK_YELLOW = new GridPoint2(1720, 710);
+    public static final GridPoint2 CARD_BRANCHSTACK_RED = new GridPoint2(1720, 540);
+    public static final GridPoint2 CARD_BRANCHSTACK_BLUE = new GridPoint2(1720, 370);
+    public static final GridPoint2 CARD_BRANCHSTACK_PURPLE = new GridPoint2(1720, 200);
+
+    public static final GridPoint2 CARD_PUBLICSTACK_GREEN = new GridPoint2(50, 370);
+    public static final GridPoint2 CARD_PUBLICSTACK_YELLOW = new GridPoint2(220, 370);
+    public static final GridPoint2 CARD_PUBLICSTACK_RED = new GridPoint2(390, 370);
+    public static final GridPoint2 CARD_PUBLICSTACK_BLUE = new GridPoint2(50, 200);
+    public static final GridPoint2 CARD_PUBLICSTACK_PURPLE = new GridPoint2(220, 200);
+
+    public static final GridPoint2 CARD_DRAWSTACK = new GridPoint2(390, 200);
+
 }
